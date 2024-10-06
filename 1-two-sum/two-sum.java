20f7dca2602cc43e5f50b4f94132c92ae2175ba0
Mayur Kandalkar <40908920+Mkknowledge@users.noreply.github.com>
2024-10-07 01:54:54 +0530

    Time: 6 ms (55.12%) | Memory: 44.8 MB (47.04%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> mapOfElement = new HashMap<>();
-        int[] arr = IntStream.range(0, nums.length)
-        .filter(index -> {
-            if (mapOfElement.containsKey(nums[index])) {
-                return true;
-            } else {
-                mapOfElement.put(target - nums[index], index);
-                return false;
-            }
-        })
-        .mapToObj(index -> new int[]{mapOfElement.get(nums[index]), index})
-        .findFirst()
-        .orElse(new int[2]);
-        return arr;
+        Map<Integer, Integer> map = new HashMap<>();
+       int[] arr = IntStream.range(0, nums.length)
+       .filter(index -> {
+        if(map.containsKey(nums[index])) {
+            return true;
+        } else {
+            map.put(target - nums[index], index);
+            return false;
+        }
+       })
+       .mapToObj(index -> new int[]{map.get(nums[index]), index})
+       .findFirst()
+       .orElse(new int[2]);
+       return arr;
     }
 }
